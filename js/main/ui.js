c29c93cf33b3df8efaeb12a3f2166f56de0d05cf
Auric Vente <mprops@protonmail.com>
2022-07-20 06:33:20 -0600

    Mods

--- js/main/ui.js
+++ js/main/ui.js
@@ -1,10 +1,8 @@
 // Arrange items depending on space
 App.setup_ui = function () {
-  let width_1 = App.el("#filter_container").offsetWidth
-  let width_2 = App.el("#buttons_container").offsetWidth
-  let width_3 = App.el("#main").offsetWidth
-
-  if (width_1 + width_2 < width_3) {
-    App.el("#top_container").classList.add("top_container_row")
+  let top = App.el("#top_container")
+  
+  if (top.scrollWidth > top.clientWidth) {
+    App.el("#top_container").classList.add("top_container_column")
   }
 }
